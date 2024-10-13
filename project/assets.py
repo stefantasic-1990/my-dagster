4af1b07095b4969657479ab6254b23f63fa4ba8b
Stefan Tasic <stefantasic@outlook.com>
2024-10-12 21:15:51 -0400

    asset and job available in UI

--- project/assets.py
+++ project/assets.py
@@ -1,1 +1,7 @@
 from dagster import asset
+from resources import SFTPResource
+
+@asset
+def source_json_sftp(sftp_source: SFTPResource) -> None:
+    file_list = sftp_source.listdir("/incoming/")
+    return file_list
--- project/definitions.py
+++ project/definitions.py
@@ -1,7 +1,14 @@
-from dagster import Definitions
+from dagster import Definitions, define_asset_job
 from resources import SFTPResource
+from assets import source_json_sftp
+
+ingest_source_data = define_asset_job(
+    name="ingest_source_data", selection="source_json_sftp"
+)
 
 defs = Definitions(
+    assets=[source_json_sftp],
+    jobs=[ingest_source_data],
     resources={
         "sftp_source": SFTPResource(host="dagster-source-sftp", username="dagster-user", password="dagster-password")
     },
